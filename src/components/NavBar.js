fb79223a999f9800ebf13fbb5a3798ff283737f6
NiftyCoffee <zoetayss@gmail.com>
2023-03-06 23:15:33 +1100

    Built home banner and nav bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav class="nav-bar">
+            <h2 class="my-name">Zoe</h2>
+            <ul>
+                <li>About</li>
+                <li>Experience</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+};
+
+export default NavBar;
