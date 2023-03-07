1e4647da275c29bf18106d3f4c4e0a63b8f84526
NiftyCoffee <zoetayss@gmail.com>
2023-03-07 15:23:11 +1100

    Added animation to photo and made nav bar fixed.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,12 +6,17 @@ const Banner = () => {
     return (
         <header class="banner">
             <NavBar/>
-            <div>
+            <div class="banner-text">
                 <h1>Hi, I'm <span class="my-name">Zoe</span>.</h1>
                 <h4>Welcome to my personal portfolio.</h4>
                 <p>I'm a second year Computer Science student based in Melbourne.</p>
             </div>
-            <img class="profile_pic" src={profile_pic}/>
+            <div class="banner-img">
+                <img class="profile_pic" src={profile_pic}/>
+                <span class="profile_border">
+                    
+                </span>
+            </div>
         </header>
     );
 }
