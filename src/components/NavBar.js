60b3d0327889cc887ae572eba47548b86a4b89d6
NiftyCoffee <zoetayss@gmail.com>
2023-03-08 12:39:19 +1100

    Fixed code for image hover effect.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,10 @@ const NavBar = () => {
         <nav class="nav-bar">
             <h2 class="my-name">Zoe</h2>
             <ul>
-                <li>About</li>
-                <li>Experience</li>
-                <li>Projects</li>
-                <li>Contact</li>
+                <li><a>About</a></li>
+                <li><a>Experience</a></li>
+                <li><a>Projects</a></li>
+                <li><a>Contact</a></li>
             </ul>
         </nav>
     )
