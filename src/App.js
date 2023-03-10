334017532e711631849cbe8d2233102458f9e827
NiftyCoffee <zoetayss@gmail.com>
2023-03-10 14:27:04 +1100

    Added about section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Banner from './components/Banner.js';
+import About from './components/About.js';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+      <About/>
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,8 @@ const Banner = () => {
             <div class="banner-text">
                 <h1>Hi, I'm <span class="my-name">Zoe</span>.</h1>
                 <h4>Welcome to my personal portfolio.</h4>
-                <p>I'm a second year Computer Science student based in Melbourne.</p>
+                <p>I'm a front-end developer and second year Computer Science student based in Melbourne.</p>
+                <p>I like learning & tech.</p>
             </div>
             <div class="banner-img">
                 <img class="profile_pic" src={profile_pic}/>
